e4cbca235f08267eda6423f1e8da7d44bf06fd05
Onyewuchi Emeka <marrnuel123@.com>
2024-01-02 18:08:27 +0100

    sub(): destructuring

--- Interview-Questions/destructuring.js
+++ Interview-Questions/destructuring.js
@@ -4,3 +4,20 @@ const [one, two, three] = foo;
 console.log(one);
 console.log(two);
 console.log(three);
+
+// swapping variables
+let a = 1;
+let b = 2;
+
+[a, b] = [b, a];
+console.log(a);
+console.log(b);
+
+// object destructuring
+const object = {
+  myName: "Manuel",
+  age: "21",
+};
+const { myName, age } = object;
+console.log(myName);
+console.log(age);
